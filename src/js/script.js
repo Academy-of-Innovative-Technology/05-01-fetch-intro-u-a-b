862e37a9f6f8af2ef147e3e3b0e80e3b2f5b0321
u-a-b <uamayabanegas26@aoiths.org>
2026-03-16 14:05:12 -0400

    ??

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,29 @@
+let cardName = document.querySelector(".card-title");
+let cardEmail = document.querySelector(".card-text");
+
 
 
 function getUserData() {
 
-
-
+fetch('../src/data/api.json')
+.then(function(response){
+  return response;  
+})
+.then(function(data){ 
+        processData(data)
+    }); 
 };
 
 
 
-function updateUserCard(user) {
-
-
-
+function processData(data) {
+console.log(data);
+cardName.innerHTML = data.firstName + " " + data.lastName;
+cardEmail.innerHTML = data.email;
 };
 
+document.querySelector("#next-button").addEventListener("click", function(processData(data));
+);
 
 // Load the first user when the page loads 
 window.onload = getUserData;
